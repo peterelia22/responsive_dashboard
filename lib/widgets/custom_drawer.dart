323148c98a2164c7bba774557936c7003dfec010
peterelia22 <peterelia914@gmail.com>
2024-07-09 15:11:11 +0300

    issues with custom drawer

--- lib/widgets/custom_drawer.dart
+++ lib/widgets/custom_drawer.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:responsive_dashboard/models/drawer_item_model.dart';
 import 'package:responsive_dashboard/utils/assets.dart';
 import 'package:responsive_dashboard/widgets/drawer_item_list_view.dart';
+import 'package:responsive_dashboard/widgets/inactive_drawer_item.dart';
 import 'package:responsive_dashboard/widgets/user_list_tile.dart';
 
 class CustomDrawer extends StatelessWidget {
@@ -10,11 +12,35 @@ class CustomDrawer extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       color: Colors.white,
-      child: Column(
-        children: [
-          const UserListTile(
-              tittle: 'jj', subTittle: 'dd', image: Assets.imagesAvatar),
-          DrawerItemListView()
+      child: CustomScrollView(
+        slivers: [
+          const SliverToBoxAdapter(
+            child: UserListTile(
+                tittle: 'jj', subTittle: 'dd', image: Assets.imagesAvatar),
+          ),
+          const SliverToBoxAdapter(
+            child: SizedBox(
+              height: 8,
+            ),
+          ),
+          DrawerItemListView(),
+          SliverFillRemaining(
+            hasScrollBody: false,
+            child: Column(
+              children: [
+                const Expanded(child: SizedBox()),
+                InActiveDrawerItem(
+                    drawerItemModel: DrawerItemModel(
+                        image: Assets.imagesSetting2, title: 'Setting System')),
+                InActiveDrawerItem(
+                    drawerItemModel: DrawerItemModel(
+                        image: Assets.imagesLogout, title: 'Logout Account')),
+                const SizedBox(
+                  height: 48,
+                )
+              ],
+            ),
+          )
         ],
       ),
     );
